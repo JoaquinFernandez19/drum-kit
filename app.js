ed7584928675bccec73e54dcc2a302fc428d4e32
Joaquin Fernandez <copako12@gmail.com>
2020-02-29 17:21:32 -0300

    testing

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 
 
-
+/*
 window.addEventListener('keydown',function(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
@@ -9,14 +9,32 @@ window.addEventListener('keydown',function(e){
     audio.play();   
     key.classList.add('playing');   
 });
+*/
+
+//mobile 
+window.addEventListener('click',function(e){
+    let currentKey = e.target.getAttribute("value");
+    if(currentKey === '65'){
+        audio65.currentTime = 0;
+        audio65.play();
+    }
+
+
+
+});
 
 function removeTransition(e){
     if (e.propertyName !== 'transform') return; //skip it if is not a transform
     this.classList.remove('playing');
-    
 }
 
 const keys = document.querySelectorAll('.key');
 
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
+
+
+
+// Audios cavernicola
+
+const audio65 = document.querySelector(`audio[data-key='65']`);
